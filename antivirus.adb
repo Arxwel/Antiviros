3039e538d1456b89412ec3d2e61bc0ec4ef95878
Arxwel <romainpasdeloup@outlook.com>
2017-01-17 20:23:39 +0100

    Update antivirus.adb
    Commentaires

--- antivirus.adb
+++ antivirus.adb
@@ -5,42 +5,40 @@ use p_virus.p_Piece_IO;
 procedure antivirus is
 
 f : p_Piece_IO.file_type;
-nb, pad : integer;
-V : TV_Virus;
-dp : string (1..2); 
+nb,pad : integer;
+dp : String(1..2);
+V : TV_Virus; 
 
 begin
 
-	Open(f, in_file, "Parties");
-	ecrire("N° config ? : "); lire(nb);
-	CreeVectVirus(f, nb, V);
-	AfficheVectVirus(V);
-	AfficheGrille(V);
-ecrire(Possible(V,rouge,bd));
-ecrire(Possible(V,jaune,hg));
-ecrire(Possible(V,rouge,hg));
+	Open(f, in_file, "Parties"); -- Ouverture du Fichier contenant les différents niveaux
+	ecrire("N° config ? : "); lire(nb); -- Lecture du Numéro de Niveau désiré
+	CreeVectVirus(f, nb, V);  -- Création du vecteur contenant les pièces du niveau et leur emplacement
+	 --AfficheVectVirus(V); -- Affichage des valeurs du vecteur
+	AfficheGrille(V); -- Affichage d'une grille de jeu avec les données du Vecteur
 
-	While not Gueri(V) loop
-		ecrire("N° pièce à déplacer ? : (0 à 8)"); lire(pad);
-		if (pad > 8) or (pad < 0) then
-			Raise EX_Piece;
+	While not Gueri(V) loop -- Permettre de faire plusieurs déplacements dans une partie
+		ecrire("N° pièce à déplacer ? : (0 à 8)"); lire(pad); -- Lecture du Numéro de Pièce à déplacer
+		if (pad > 8) or (pad < 0) then -- On vérifie que le numéro de pièce saisi est valide
+			Raise EX_Piece;		-- Si non déclenchement d'une exception
 		end if;
 			
-		ecrire("Direction du déplacement ? : (bg, hg, bd ou hd)"); lire(dp);
+		ecrire("Direction du déplacement ? : (bg, hg, bd ou hd)"); lire(dp); -- Lecture du type de déplacement désiré pour la pièce
 
-		if not ((dp = "bg") or (dp = "hg") or (dp = "bd") or (dp ="hd")) then
-			Raise EX_Dir;
+		if not ((dp = "bg") or (dp = "hg") or (dp = "bd") or (dp ="hd")) then -- On vérifie que le déplacement saisi existe bien
+			Raise EX_Dir; -- Si non déclenchement d'une exception
 		end if;
 
-		if Presente(V, T_Piece'Val(pad)) and then Possible(V, T_Piece'Val(pad), T_Direction'Value(dp)) then 
-			Deplacement(V, T_Piece'Val(pad), T_Direction'Value(dp));
+		if Presente(V, T_Piece'Val(pad)) and then Possible(V, T_Piece'Val(pad), T_Direction'Value(dp)) then -- On teste si la pièce existe bien dans la grille
+															-- Et que le déplacement est possible
+			Deplacement(V, T_Piece'Val(pad), T_Direction'Value(dp)); -- Si oui on effectue le déplacement
 		else
-			ecrire("error");
+			ecrire("Erreur, pièce inexistante dans la grille ou le déplacement est impossible"); -- Si non Message d'erreur
 		end if;
-			AfficheGrille(V);
+			AfficheGrille(V); -- Affichage de la grille de jeu après déplacement
 	end loop; 
-	Close(f);
-exception 
+	Close(f); -- Fermeture du Fichier de Niveaux
+exception -- Traitement des exceptions
 			when Ex_Piece =>
 							ecrire_ligne("La valeur de la pièce doit être comprise 0 et 8");
 			when Ex_Dir =>
