a97c7829a3acc3e9f0548a1021cbf96aec54d206
Arxwel <romainpasdeloup@outlook.com>
2017-01-17 21:18:09 +0100

    Update p_vuegraph.ads

--- p_vuegraph.ads
+++ p_vuegraph.ads
@@ -6,7 +6,7 @@ with p_esiut; use p_esiut;
 package p_vuegraph is
 
 procedure FenetreChoix (f : in out p_Piece_IO.file_type; V : in out TV_Virus);
---{f ouvert, InitialiserFenetres a deja ete fait} => {Creation d'une fenetre de saisi de niveau, qui initialise un vecteur V avec
+--{f ouvert, f-=<>, InitialiserFenetres a deja ete fait} => {Creation d'une fenetre de saisi de niveau, qui initialise un vecteur V avec
                                                       --les donnees de f pour le niveau saisi}
 function Color (C: in T_Piece) return FL_PD_COL;
 --{} => {Renvoie la couleur en FL_PD_COL correspondant a la Couleur T_Piece fournie}
