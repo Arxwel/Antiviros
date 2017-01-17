df0efe8a97eaed6fa27eef677df411c21f7e169f
Arxwel <romainpasdeloup@outlook.com>
2017-01-17 19:59:28 +0100

    Update p_virus.ads

--- p_virus.ads
+++ p_virus.ads
@@ -60,17 +60,9 @@ function Gueri (V : in TV_Virus) return Boolean;
 function Presente (V : in TV_Virus; Coul : in T_Piece) return Boolean;
 -- {} => {resultat =  la piece de couleur Coul appartient a V}
 
---function Libre (V : in TV_Virus; i : in T_Lig; j : in T_Col; Dir : in T_Direction) return boolean;
----- {} => {resultat = indique si la case n'est pas occupée pour le deplacement que l'on veut effectuer}
-
---function Possible (V : in TV_Virus; Coul : in T_Piece; Dir : in T_Direction) return Boolean;
----- {P appartient a la grille V} => {resultat = vrai si la piece de couleur Coul peut etre 
-----                                             deplacee dans la direction Dir}
-
 function Possible (V : in TV_Virus; Coul : in T_Piece; Dir : in T_Direction) return Boolean;
-
---procedure MAJ (V : in out TV_Virus; Dir : in T_Direction; Vnew: in out TV_Virus);
----- {le deplacement doit être possible et la couleur doit être presente} => {met à jour V après déplacement}
+-- {P appartient a la grille V} => {resultat = vrai si la piece de couleur Coul peut etre 
+--                                             deplacee dans la direction Dir}
 
 procedure Deplacement(V : in out TV_Virus; Coul : in T_Piece; Dir :in T_Direction);
 -- {la piece de couleur Coul peut etre deplacee dans la direction Dir} 
